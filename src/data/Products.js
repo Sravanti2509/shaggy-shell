fcc285995e86a1701c11d3434b17216da87ee8a3
P.Naga Sai Sravanti <sravanthipuvvala74@gmail.com>
2026-08-07 23:59:24 +0530

    Initial Sravs Bakery website

---
+++ src/data/Products.js
@@ -0,0 +1,94 @@
+const cakes = [
+  {
+    name: "Premium butterscotch pastry",
+    price: "55",
+    image: "/products/Premium-butterscotch-pastry.jpg"
+  },
+  {
+    name: "Premium Pineapple Pastry",
+    price: "60",
+    image: "/products/Premium-Pineapple-Pastrie.jpg"
+  },
+  {
+    name: "Premium Chocolate Pastry",
+    price: "60",
+    image: "/products/Premium-Chocolate-Pastry.jpg"
+  },
+  {
+    name: "Aloo Puff",
+    price: "20",
+    image: "/products/Aloo-Puff.jpg"
+  },
+  {
+    name: "Chicken Puff",
+    price: "35",
+    image: "/products/Chicken-Puff.jpg"
+  },
+  {
+    name: "Paneer Puff",
+    price: "25",
+    image: "/products/Paneer-Puff.jpg"
+  },
+  {
+    name: "Egg Puff",
+    price: "25",
+    image: "/products/Egg-Puff.jpg"
+  },
+  {
+    name: "Cookies",
+    price: "60",
+    image: "/products/cookies.jpg"
+  },
+  {
+    name: "Cheesecake",
+    price: "65",
+    image: "/products/cheesecake.jpg"
+  },
+  {
+    name: "Chicken Sandwich",
+    price: "60",
+    image: "/products/Chicken-Sandwich.jpg"
+  },
+  {
+    name: "Cinnamon Roll",
+    price: "60",
+    image: "/products/cinnamon-roll.jpg"
+  },
+  {
+    name: "Vanilla Milk Shake",
+    price: "60",
+    image: "/products/Vanilla-Milk-Shake.jpg"
+  },
+  {
+    name: "Croissant",
+    price: "60",
+    image: "/products/croissant.jpg"
+  },
+  {
+    name: "Donut",
+    price: "60",
+    image: "/products/donut.jpg"
+  },
+  {
+    name: "Strawberry Milk Shake",
+    price: "60",
+    image: "/products/strawberry-milk-shake.jpg"
+  },
+  {
+    name: "Mongo Milk Shake",
+    price: "60",
+    image: "/products/Mongo-Milk-shake.jpg"
+  },
+  {
+    name: "Paneer Sandwich",
+    price: "60",
+    image: "/products/Paneer-Sandwich.jpg"
+  },
+  {
+    name: "Veg Sandwich",
+    price: "60",
+    image: "/products/Veg-Sandwich.jpg"
+  }
+];
+
+export default cakes;
